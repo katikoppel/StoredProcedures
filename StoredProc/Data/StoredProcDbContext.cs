55e4a620fe2b159038961752657f0f42437bef08
katikoppel <katikoppel1995@gmail.com>
2025-06-04 11:02:36 +0300

    Init

---
+++ StoredProc/Data/StoredProcDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoredProc.Models;
+
+namespace StoredProc.Data
+{
+    public class StoredProcDbContext : DbContext
+    {
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
+            : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        
+    }
+}
---
+++ StoredProc/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoredProc.Models
+{
+    public class Employee
+    {
+        [Key]
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public int Salary { get; set; }
+    }
+}
